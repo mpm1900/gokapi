e6898c456c30d06bf147601506b915f2b563df84
mpm1900 <almostmacmiller@gmail.com>
2025-07-27 02:16:47 -0500

    scroll layouts

--- web/src/routes/app/index.tsx
+++ web/src/routes/app/index.tsx
@@ -14,27 +14,24 @@ function RouteComponent() {
     `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello World!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0,"textStyle":""}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`,
   )
   return (
-    <div className="@container/main max-h-full flex-1 flex flex-col overflow-hidden">
+    <div className="@container/main relative flex-1 flex flex-col overflow-hidden">
       <div>header</div>
-      <div className="flex flex-1 flex-row max-h-full">
+      <div className="flex flex-1 flex-row min-h-0">
         <Editor
-          className="flex-1 max-h-full p-4"
+          className="flex-1 p-4 min-h-0"
           value={value}
           onChange={(es) => {
             setValue(es)
           }}
         />
         <Separator orientation="vertical" />
-        <Tabs
-          defaultValue="json"
-          className="flex-1 pt-2 max-h-full overflow-auto gap-0"
-        >
-          <TabsList className="mx-4 mb-2">
+        <Tabs defaultValue="json" className="flex-1 pt-2 gap-0 min-h-0 flex flex-col">
+          <TabsList className="mx-4 mb-2" >
             <TabsTrigger value="json">JSON</TabsTrigger>
             <TabsTrigger value="treeview">TreeView</TabsTrigger>
           </TabsList>
           <Separator />
-          <TabsContent value="json" className="max-h-full overflow-auto">
+          <TabsContent value="json" className="flex-1 min-h-0 max-h-full overflow-auto">
             <pre className="p-4">{JSON.stringify(value, null, 2)}</pre>
           </TabsContent>
         </Tabs>
