3b773ef0bf138acade97f9ac11ef1b07d6600c92
mpm1900 <almostmacmiller@gmail.com>
2025-07-31 23:12:59 -0500

    confirm leave game changes

--- web/src/routes/app/$gameID.tsx
+++ web/src/routes/app/$gameID.tsx
@@ -2,13 +2,27 @@ import { GameManager } from '@/components/game/game-manager'
 import { GameSidebar } from '@/components/game/game-sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { GameProvider } from '@/hooks/use-game'
-import { createFileRoute } from '@tanstack/react-router'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogCancel,
+  AlertDialogDescription,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+import { createFileRoute, useBlocker } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/app/$gameID')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { status, proceed, reset } = useBlocker({
+    shouldBlockFn: () => true,
+    withResolver: true,
+  })
   return (
     <GameProvider>
       <SidebarProvider
@@ -21,6 +35,23 @@ function RouteComponent() {
         </SidebarInset>
         <GameSidebar />
       </SidebarProvider>
+      <AlertDialog open={status === 'blocked'}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you sure you want to leave this game?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This will disconnect you from the game and you will lose all
+              previous chat messages.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={reset}>Go Back</AlertDialogCancel>
+            <AlertDialogAction onClick={proceed}>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </GameProvider>
   )
 }
