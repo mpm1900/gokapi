f3339960b1eb7c08da9b3d497e2b6732b4701d7c
mpm1900 <almostmacmiller@gmail.com>
2025-08-02 13:04:47 -0500

    question timer

--- web/src/components/game/game-question.tsx
+++ web/src/components/game/game-question.tsx
@@ -1,14 +1,68 @@
 import { type Question, type QuestionChoice } from '@/types/game'
 import { Button } from '../ui/button'
 import { Progress } from '../ui/progress'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { cn } from '@/lib/utils'
+
+function evalQuestion(question: Question, choice: QuestionChoice) {
+  if (question.type === 'MULTIPLE_CHOICE') {
+    if (typeof question.eval === 'function') {
+      return question.eval(choice)
+    }
+    return question.eval.includes(choice)
+  }
+  return question.eval ? question.eval(choice.text) : false
+}
 
 export function GameQuestion({ question }: { question: Question }) {
+  const timer = useRef<NodeJS.Timeout>(null)
   const [selected, setSelected] = useState<QuestionChoice>()
+  const [progress, setProgress] = useState(question.time)
+  const done = progress <= 0
+  const isCorrect = selected && evalQuestion(question, selected)
+
+  function clearTimer() {
+    if (timer.current) {
+      clearInterval(timer.current)
+      timer.current = null
+    }
+  }
+
+  function startTimer() {
+    setProgress(question.time)
+    timer.current = setInterval(() => {
+      setProgress((prev) => {
+        if (prev <= 0) {
+          clearTimer()
+          return 0
+        }
+        return prev - 100
+      })
+    }, 100)
+  }
+
+  function reset() {
+    clearTimer()
+    startTimer()
+    setSelected(undefined)
+  }
+
+  useEffect(() => {
+    startTimer()
+    return () => clearTimer()
+  }, [question])
+
   return (
     <div className="flex flex-col gap-6">
-      <Progress value={80} />
-      <span className="text-4xl font-bold text-center">{question.prompt}</span>
+      <Progress value={(progress * 100) / question.time} />
+      <span
+        className={cn('text-4xl font-bold text-center', {
+          'text-green-500': done && isCorrect,
+          'text-red-500': done && !isCorrect,
+        })}
+      >
+        {question.prompt}
+      </span>
       {question.type === 'MULTIPLE_CHOICE' && (
         <div className="grid grid-cols-4 gap-2 px-16">
           {question.choices.map((choice) => (
@@ -30,9 +84,9 @@ export function GameQuestion({ question }: { question: Question }) {
           ))}
         </div>
       )}
-      {!!selected && (
+      {(!!selected || done) && (
         <div>
-          <Button variant="outline" onClick={() => setSelected(undefined)}>
+          <Button variant="outline" onClick={() => reset()}>
             Reset
           </Button>
         </div>
